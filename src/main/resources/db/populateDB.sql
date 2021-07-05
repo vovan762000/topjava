3493c55e41b67630a1b33e447ea7fb5bec42cbcd
vovan762000@gmail.com <vovan762000@gmail.com>
2021-07-05 13:37:45 +0300

    add inicialize meals table

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,5 +1,10 @@
-DELETE FROM user_roles;
-DELETE FROM users;
+DELETE
+FROM user_roles;
+DELETE
+FROM meals;
+DELETE
+FROM users;
+
 ALTER SEQUENCE global_seq RESTART WITH 100000;
 
 INSERT INTO users (name, email, password)
@@ -9,3 +14,15 @@ VALUES ('User', 'user@yandex.ru', 'password'),
 INSERT INTO user_roles (role, user_id)
 VALUES ('USER', 100000),
        ('ADMIN', 100001);
+
+INSERT INTO meals (date_time, description, calories, user_id)
+values ('30-Jan-2020 10:00', 'Завтрак', 500, 100000),
+       ('30-Jan-2020 13:00', 'Обед', 1000, 100000),
+       ('30-Jan-2020 20:00', 'Ужин', 500, 100000),
+       ('31-Jan-2020 00:00', 'Еда на граничное значение', 100, 100000),
+       ('31-Jan-2020 10:00', 'Завтрак', 1000, 100000),
+       ('31-Jan-2020 13:00', 'Обед', 500, 100000),
+       ('31-Jan-2020 20:00', 'Ужин', 410, 100000),
+       ('1-Jun-2015 10:00', 'Админ ланч', 510, 100001),
+       ('1-Jun-2015 13:00', 'Админ ланч', 1500, 100001);
+
